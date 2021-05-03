62aa787bda29865e55c345c9127f5a613b2715a2
Cedric <cedrictd.ctd@mail.com>
2021-05-03 16:03:15 +0200

    converting strings to cards

--- src/card.py
+++ src/card.py
@@ -1,4 +1,8 @@
 # %%
+import logging as lg
+
+lg.basicConfig(level=lg.DEBUG)
+# %%
 class Card:
     """A card in the standard deck of crazy8, poker and so forth."""
 
@@ -25,6 +29,17 @@ class Card:
 
         return f"{suit}{rank}"
     
+    def rank_str_to_int(rank):
+        if 11 <= rank <= 14:
+            if rank == 11:
+                return "J"
+            elif rank == 12:
+                return "Q"
+            elif rank == 13:
+                return "K"
+            elif rank == 14:
+                return "A"
+
     def __int__(self):
         return self.value
     
@@ -33,4 +48,42 @@ class Card:
 
     def get_rank(self):
         return int((self.value - self.value % 4) / 4 + 2)
+
+    def str_to_card(string):
+        """Returns the card object that corresponds to a string representation of a card.
+
+        Input (str): string representation of card. 
+        Output (Card): card object
+
+        Examples:
+        str_to_card("♥J") -> Card(37)
+        str_to_card("♣K") -> Card(46)
+        str_to_card("♥2") -> Card(1)
+        """
+        suit_str, rank_str = string[0], string[1:]
+        lg.debug(f"Processing card with suit_str={suit_str} and rank_str={rank_str}")
+
+        suit_int = Card.suits.find(suit_str)
+        if suit_int == -1:
+            raise ValueError("Card string must start with one of [♠♥♣♦]")
+            return
+        
+        try:
+            rank_int = int(rank_str)
+        except ValueError:
+            if rank_str == "J":
+                rank_int= 11
+            elif rank_str == "Q":
+                rank_int= 12
+            elif rank_str == "K":
+                rank_int= 13
+            elif rank_str == "A":
+                rank_int= 14
+            else:
+                raise ValueError(("Invalid rank"))
+
+        value = (rank_int - 2) * 4 + suit_int
+        return Card(value)
+
+# TODO: more exeptions
 # %%
